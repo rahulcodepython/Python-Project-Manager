812b9d2c165e43b6c8d2b17be24545d50510bb96
Rahul Das <rahulcodepython@gmail.com>
2024-10-25 17:54:00 +0530

    MODIFY: FIles & folders are not being created.

--- ppm/ppm/src/functions/init.py
+++ ppm/ppm/src/functions/init.py
@@ -7,6 +7,7 @@ class Init():
         self.current_working_directory: str = os.getcwd()
         self.current_folder_name: str = os.path.basename(
             self.current_working_directory)
+        self.entry_point_path = f"./src/"
         self.project_name = self.current_folder_name.lower()
         self.version = "1.0.0"
         self.description = ""
@@ -15,6 +16,9 @@ class Init():
         self.author = ""
         self.license = "ISC"
         self.agree_to_create_ppm_toml = "yes"
+        self.python_version = sys.version.split(" ")[0]
+        self.virtual_environment_path = "./venv/Scripts/activate"
+        self.meta_data_file_name = "ppm.toml"
 
     def valided_user_input(self, prompt: str, default: str) -> str:
         try:
@@ -25,8 +29,8 @@ class Init():
             sys.exit(0)
 
     def get_user_input(self) -> None:
-        print("""
-This utility will walk you through creating a ppm.toml file.
+        print(f"""
+This utility will walk you through creating a {self.meta_data_file_name} file.
 It only covers the most common items and meta data of the project.
         """)
         print("Press ^C at any time to quit.")
@@ -43,18 +47,16 @@ It only covers the most common items and meta data of the project.
             "git repository", self.git_repository)
         self.author = self.valided_user_input("author", self.author)
         self.license = self.valided_user_input("license", self.license)
+        self.entry_point_path += self.entry_point
 
         # Confirm and show details
         print(f"\nAbout to write to {
-              self.current_working_directory}\\ppm.toml:")
+              self.current_working_directory}\\{self.meta_data_file_name}:")
         self.agree_to_create_ppm_toml = self.valided_user_input(
             "Is this ok?", self.agree_to_create_ppm_toml)
 
-        print(self.agree_to_create_ppm_toml, self.project_name, self.version,
-              self.description, self.entry_point, self.git_repository, self.author, self.license)
-
     def create_ppm_toml(self) -> None:
-        with open("ppm.toml", "w") as file:
+        with open(self.meta_data_file_name, "w") as file:
             file.write(f"""[project]
 name = "{self.project_name}"
 version = "{self.version}"
@@ -65,12 +67,64 @@ author = "{self.author}"
 license = "{self.license}"
 
 
+[python]
+python_version = "{self.python_version}"
+
+
+[environment]
+path = "{self.virtual_environment_path}"
+
+
+[environment_variables]
+path = "./env"
+
+
+[command]
+run = "python {self.entry_point_path}"
+
+
 [dependencies]
 """)
-        print("ppm.toml created")
+        print(f"\n{self.current_working_directory}\\{
+            self.meta_data_file_name} created.")
+        print("This python project is built on python version", self.python_version)
+        print("\nCongratulations! Your project is ready to go.")
+        print("\nTo install the dependencies, use the command 'ppm install'")
+        print("To run the project, use the command 'ppm run'")
+        print("To activate the virtual environment, use the command 'ppm activate'")
+        print("To deactivate the virtual environment, use the command 'ppm deactivate'")
+        print(f"\nmain.py file is created in src folder ({
+            self.entry_point_path}). You can start coding in main.py file.")
+        print("\nHappy coding!")
+
+    def create_env_file(self) -> None:
+        with open(".env", "w") as file:
+            file.write("")
+
+    def create_project_folder_files(self) -> None:
+        if not os.path.exists("src"):
+            os.makedirs("src")
+
+        with open("src/main.py", "w") as file:
+            file.write("""from dotenv import load_dotenv
+import os
+                        
+load_dotenv()
+                    
+# to use environment variables
+# os.getenv('ENV_VARIABLE_NAME')
+                    
+def main() -> None:
+    print('Hello, World!')
+                    
+if __name__ == '__main__':
+    main()
+""")
 
     def init(self) -> None:
         self.get_user_input()
-        self.create_ppm_toml() if (self.agree_to_create_ppm_toml.lower(
-        ) == "yes" or self.agree_to_create_ppm_toml.lower(
-        ) == "y") else print("Operation is terminated.")
+
+        if (self.agree_to_create_ppm_toml.lower() == "yes" or self.agree_to_create_ppm_toml.lower() == "y"):
+            self.create_env_file()
+            self.create_project_folder_files()
+            self.create_ppm_toml()
